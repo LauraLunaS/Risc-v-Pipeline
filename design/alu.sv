c687c4a891ae6cd167324eec1c5cd79e1f061f5b
Laura Luna <lauraluna.siqueira@hotmail.com>
2025-07-18 16:46:53 -0300

    feat: new instruction SLL

--- design/alu.sv
+++ design/alu.sv
@@ -25,6 +25,8 @@ module alu#(
 		    ALUResult = SrcA - SrcB;
 	    4'b0100:        //XOR
 		    ALUResult = SrcA ^ SrcB;
+            4'b0101:        // SLL 
+                    ALUResult = SrcA << SrcB;
             4'b1000:        // Equal
                     ALUResult = (SrcA == SrcB) ? 1 : 0;
             default:
